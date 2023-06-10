19102c85749fa5b1caadfd34e38fcd0c8f996bb9
Pnoccio <robinshango@gmail.com>
2023-06-10 15:31:36 +0300

    creating a dashboard ui

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
+    Route::get('dashboard', 'Admincontroller@Dashboard');
+});
