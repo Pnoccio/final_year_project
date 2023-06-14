fc07701b92a39286633b30019db4d3ced3170bb4
Pnoccio <robinshango@gmail.com>
2023-06-14 17:40:04 +0300

    admin Table and putting dummy data for admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\Admin\AdminController;
 
 
 
@@ -9,6 +10,9 @@ Route::get('/', function () {
 });
 
 Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
-    Route::get('dashboard', 'AdminController@Dashboard');
-    Route::match(['get', 'post'], 'Admincontroller@login');
+    // Route::get('dashboard', 'AdminController@Dashboard');
+    Route::match(['get', 'post'], 'login', 'AdminController@login');
+    Route::group(['middleware'=>['admin']], function(){
+        Route::get('dashboard', 'AdminController@Dashboard');
+    });
 });
